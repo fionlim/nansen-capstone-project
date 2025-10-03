16ca709bf232dff2f88cc1d20467e926c79c23f9
aseah15 <102alvin@gmail.com>
2025-10-04 02:15:48 +0800

    feat: Add functions to get all or n pages: - _post_all_pages() calls _post() till last page is reached - _post_n_pages() calls _post() till specified page number is reached or if last page is reached before then - Updated smart_money_netflow() and smart_money_dex_trades() to call new functions - Cleaned up functions that were unused

--- nansen_client.py
+++ nansen_client.py
@@ -21,15 +21,43 @@ class NansenClient:
         resp.raise_for_status()
         data = resp.json()
         return data
+    
+    def _post_all_pages(self, payload: Dict, path: str):
+        all_items = []
+        while True:
+            response = self._post(path, payload)
+            items = response.get("data", [])
+            all_items.extend(items)
+            if response["pagination"]["is_last_page"] is True:
+                break
+            payload["pagination"]["page"] += 1
+        return all_items
+    
+    def _post_n_pages(self, payload: Dict, path: str, n: int):
+        all_items = []
+        for _ in range(n):
+            response = self._post(path, payload)
+            items = response.get("data", [])
+            all_items.extend(items)
+            if response["pagination"]["is_last_page"] is True:
+                break
+            payload["pagination"]["page"] += 1
+        return all_items
 
-    def smart_money_netflow(self, payload: Dict):
-        return self._post("/smart-money/netflow", payload)
+    def smart_money_netflow(self, payload: Dict, fetch_all: bool = False, n: int = 1):
+        if fetch_all:
+            return self._post_all_pages(payload, "/smart-money/netflow")
+        elif n > 1:
+            return self._post_n_pages(payload, "/smart-money/netflow", n)
+        else:
+            return self._post("/smart-money/netflow", payload).get("data", [])
+    
+    def smart_money_dex_trades(self, payload: Dict, fetch_all: bool = False, n: int = 1):
+        if fetch_all:
+            return self._post_all_pages(payload, "/smart-money/dex-trades")
+        elif n > 1:
+            return self._post_n_pages(payload, "/smart-money/dex-trades", n)
+        else:
+            return self._post("/smart-money/dex-trades", payload).get("data", [])
 
-    def smart_money_holdings(self, payload: Dict):
-        return self._post("/smart-money/holdings", payload)
-
-    def tgm_token_screener(self, payload: Dict):
-        return self._post("/token-screener", payload)
-
-    def tgm_flow_intelligence(self, payload: Dict):
-        return self._post("/tgm/flow-intelligence", payload)
+    # Add more function as needed for other endpoints below :D
